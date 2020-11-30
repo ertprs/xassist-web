be484aa8c869af9221f9fd1268de7e839e5f7e65
gasquarebros <gasquarebros@gmail.com>
2020-11-30 11:39:01 +0530

    login, signup and reset password screen HTML done

--- example/js/example.js
+++ example/js/example.js
@@ -66,4 +66,108 @@ function subscribeToInstantLogin() {
 $('.signup-link').click(function() {
    $('.signup-section').show();
    $('.login-section').hide();
+   $('.resetpassword-section').hide();
 });
+
+$('.forgot-password').click(function() {
+   $('.signup-section').hide();
+   $('.login-section').hide();
+   $('.resetpassword-section').show();
+});
+
+$('.bcklogin').click(function() {
+   $('.signup-section').hide();
+   $('.login-section').show();
+   $('.resetpassword-section').hide();
+})
+
+
+$('#signup-form').submit(function() {
+   var isFormValid = validateInput('signup-section');
+   if (!isFormValid) { 
+      return false;
+   } else {
+      var emailValue = $('#signup_email').val();
+      isFormValid = ValidateEmail(emailValue, 'signup-email-error'); 
+      if(isFormValid) {
+         var password = $('#signup_password').val();
+         var cpassword = $('#signup_cpassword').val();
+         isFormValid = confirmPasswordValidation(password, cpassword, 'signup-cpassword-error');
+         if(isFormValid) {
+            return true;
+         } else {
+            return false;
+         }
+      } else {
+         return false;
+      }
+   }
+});
+
+$('#reset-form').submit(function() {
+   var isFormValid = validateInput('resetpassword-section');
+   if (!isFormValid) { 
+      return false;
+   } else {
+      var emailValue = $('#reset_email').val();
+      isFormValid = ValidateEmail(emailValue, 'reset_email_error'); 
+      if(isFormValid) {
+         var password = $('#reset_password').val();
+         var cpassword = $('#reset_cpassword').val();
+         isFormValid = confirmPasswordValidation(password, cpassword, 'reset_cpassword_error');
+         if(isFormValid) {
+            return true;
+         } else {
+            return false;
+         }
+      } else {
+         return false;
+      }
+   }
+});
+
+
+function validateInput(section) {
+   var isFormValid = true;
+   $("."+ section + " input").each(function(){
+      if ($.trim($(this).val()).length == 0){
+         $(this).addClass("highlight");
+         isFormValid = false;
+      }
+      else{
+         $(this).removeClass("highlight");
+      }
+  });
+  return isFormValid;
+}
+
+function ValidateEmail(inputText, elementClass)
+{
+   var mailformat = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+   if(inputText.match(mailformat))
+   {
+      $('.'+elementClass).hide();
+      return true;
+   }
+   else
+   {
+      $('.'+elementClass).show();
+      $('.'+elementClass).text('Invalid Email Address');
+      return false;
+   }
+}
+
+function confirmPasswordValidation(inputText, CinputText, elementClass)
+{
+   if(inputText === CinputText)
+   {
+      $('.'+elementClass).hide();
+      return true;
+   }
+   else
+   {
+      $('.'+elementClass).show();
+      $('.'+elementClass).text('Password and Confirm password are not same');
+      return false;
+   }
+}
